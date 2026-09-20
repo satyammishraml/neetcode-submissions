49ec904d148a0b8b57a291f080f18dca07dfdf84
satyammishraml <7990satyam200@gmail.com>
2026-09-20 23:04:44 +0530

    Add: buy-and-sell-crypto - submission-9

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-9.py
@@ -0,0 +1,67 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buying_pointer = 0
+        selling_pointer = 1
+        max_profit = 0
+        while selling_pointer < len(prices):
+            if prices[selling_pointer] > prices[buying_pointer]:
+                max_profit = max(max_profit, prices[selling_pointer]- prices[buying_pointer])
+            else:
+                buying_pointer = selling_pointer
+            selling_pointer+=1
+        return max_profit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""""
+# Method 1
+
+        buying_price = float("inf")
+        max_profit = 0
+        for price in prices:
+            if price <  buying_price:
+                buying_price = price
+            max_profit = max(price - buying_price, max_profit)
+        return max_profit
+
+
+1. 
+buying_price = float(inf)
+for price in prices:
+    if price < buying_price:
+        buying_price = price
+2.
+max_profit = 0
+for selling_price in prices:
+    profit = selling_price - buying_price
+    max_profit = max(profit, max_profit)
+
+3.Merge 
+buying_price = float(inf)
+profit = 0
+for price in prices:
+    if price < buying_price:
+       price =  buying_price
+    max_profit = max(profit, price - buying_price)
+return max_profit
+
+
+"""
+
+
+
+
+
